de9982b00f238ea3bb190fc1a145bbfe86d2cad6
BryanMico <imbryanmicobaril@gmail.com>
2025-07-25 12:56:03 +0800

    chore: move sidebar toggle script to custom folder and update layout

---
+++ EmployeeTimeTrackingSystem/Scripts/CustomScripts/LayoutScript.js
@@ -0,0 +1,16 @@
+﻿function SidebarToggle() {
+    $('#sidebarToggle').click(function () {
+        $('#sidebar').toggleClass('collapsed');
+        $('#mainContent').toggleClass('expanded');
+
+        if ($('#sidebar').hasClass('collapsed')) {
+            $('#sidebarToggle').text('Open →');
+        } else {
+            $('#sidebarToggle').text('← Hide');
+        }
+    });
+}
+
+$(document).ready(function () {
+    SidebarToggle();
+});
